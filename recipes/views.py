fb05a194afc7346fc93bf2ecf96439fbf44ee9b4
Jadson-Jose <jadsonjosesilva@gmail.com>
2024-12-02 12:29:56 -0300

    Error 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
@@ -16,8 +17,13 @@ def category(request, category_id):
         category__id=category_id,
         is_published=True
     )
+
+    if not recipes:
+        raise Http404('Page not found ')
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category |'
     })
 
 
