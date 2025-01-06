80e04c56c59e7802492d1557f2e3c2b8ffab3147
Jadson-Jose <jadsonjosesilva@gmail.com>
2025-01-06 13:14:00 -0300

    sinc

--- recipes/views.py
+++ recipes/views.py
@@ -36,4 +36,8 @@ def recipe(request, id):
 
 
 def search(request):
-    return render(request, 'recipes/pages/search.html')
+    search_term = request.GET.get('q', '').strip()
+    if not search_term:
+        raise Http404()
+    return render(request, 'recipes/pages/search.html',
+                  {'page_title': f'Search for "{search_term}" |'},)
