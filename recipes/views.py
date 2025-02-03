e4e70b87066e983ad4a8ed3fba337fcb09541743
Jadson-Jose <jadsonjosesilva@gmail.com>
2025-02-03 14:59:44 -0300

    sinc

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from recipes.models import Recipe
 from django.db.models import Q
 from django.core.paginator import Paginator
+from utils.pagination import make_pagination_range
 
 
 def home(request):
@@ -10,12 +11,24 @@ def home(request):
         is_published=True,
     ).order_by('-id')
 
-    current_page = request.GET.get('page', 1)
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
     paginator = Paginator(recipes, 9)
     page_obj = paginator.get_page(current_page)
 
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
     return render(request, 'recipes/pages/home.html',
-                  context={'recipes': page_obj})
+                  context={'recipes': page_obj,
+                           'pagination_range': pagination_range
+                           })
 
 
 def category(request, category_id):
