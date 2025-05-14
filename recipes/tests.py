a6f4b4a3f1e1d7817ccb71aab1ba58c57842ac71
Jadson-Jose <jadsonjosesilva@gmail.com>
2025-05-14 15:27:17 -0300

    feat: sinc

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from recipes import views
 
 
 class RecipeURLsTest(TestCase):
@@ -15,3 +16,9 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
+        
+class RecipeViwesTest(TestCase):
+    def test_recipe_home_views_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+        
