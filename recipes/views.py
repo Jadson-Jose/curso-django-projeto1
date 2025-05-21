445ad95f874e328f6c7832e05a64a1cb5acdef23
Jadson-Jose <jadsonjosesilva@gmail.com>
2025-05-21 13:46:16 -0300

    feat: sinc

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe, Category
 from django.http import Http404
+from django.db.models import Q
 
 
 def home(request):
@@ -40,6 +41,13 @@ def search(request):
     search_term = request.GET.get('q', '').strip()
     if not search_term:
         raise Http404()
-    return render(request, 'recipes/pages/search.html', {'page_title': f'Search for "{search_term}" | ',
-                                                         'search_term': search_term,
-                                                         })
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) | 
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+    return render(request, 'recipes/pages/search.html', 
+                  {'page_title': f'Search for "{search_term}" | ',
+                   'search_term': search_term,
+                   'recipes': recipes,
+                   })
+    
